5917457cb16bb72f5f8a26771b1b32d9ebfff848
zazazabo <z277402131@126.com>
2019-01-11 17:20:10 +0800

    111 Signed-off-by: zazazabo <z277402131@126.com>

--- web/jquery-ui-bootstrap/assets/js/demo1.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-    //####### Tooltip
-
-    $( "#tooltip" ).tooltip();
-
-    // File input (using http://filamentgroup.com/lab/jquery_custom_file_input_book_designing_with_progressive_enhancement/)
-   
-});
